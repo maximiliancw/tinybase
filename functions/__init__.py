1578b2d670baacb40b2b0aa661e813ae4dbfe008
maximiliancw <wunderkind.serie-0f@icloud.com>
2026-01-21 02:09:14 +0100

    Remove example functions generated at runtime from VCS

--- functions/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-TinyBase Functions Package
-
-User-defined functions should be placed in individual files within this package.
-Each function file can use uv's single-file script feature to define inline dependencies.
-"""
